b85f11c7840e2c474ea6e1ba9e18a946b1133a09
huiyadanli <huiyadanli@gmail.com>
2024-03-05 23:10:14 +0800

    BitBlt 获取原神窗体截图

--- src/screenshot.cpp
+++ src/screenshot.cpp
@@ -2,34 +2,48 @@
 using cv::Mat;
 
 Screen::Screen() {
-  // 获取窗口当前显示的监视器
-  HWND hWnd = GetDesktopWindow();
-  HMONITOR hMonitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
+  // 获取原神窗口的句柄
+  HWND hWnd = FindHandleByWindowName();
 
-  // 获取监视器逻辑宽度
-  MONITORINFOEX monitorInfo;
-  monitorInfo.cbSize = sizeof(monitorInfo);
-  GetMonitorInfo(hMonitor, &monitorInfo);
+  // 获取游戏区域
+  RECT client_rect = { 0, 0, 0, 0 };
+  GetClientRect(hWnd, &client_rect);
 
-  // 获取监视器物理宽度
-  DEVMODE dm;
-  dm.dmSize = sizeof(dm);
-  dm.dmDriverExtra = 0;
-  EnumDisplaySettings(monitorInfo.szDevice, ENUM_CURRENT_SETTINGS, &dm);
-  int cxPhysical = dm.dmPelsWidth;
-
-  m_width = dm.dmPelsWidth;
-  m_height = dm.dmPelsHeight;
+  m_width = client_rect.right - client_rect.left;
+  m_height = client_rect.bottom - client_rect.top;
   m_screenshotData = new char[m_width * m_height * 4];
   memset(m_screenshotData, 0, m_width);
 
-  // 获取屏幕 DC
-  m_screenDC = GetDC(NULL);
+  // 获取句柄 DC // 建议下沉至 getScreenshot() 并 DeleteDC + ReleaseDC
+  m_screenDC = GetDC(hWnd);
   m_compatibleDC = CreateCompatibleDC(m_screenDC);
 
   // 创建位图
   m_hBitmap = CreateCompatibleBitmap(m_screenDC, m_width, m_height);
   SelectObject(m_compatibleDC, m_hBitmap);
+}
+
+/** 
+ * 获取原神窗口的句柄
+ * 此方法获取的句柄有概率造成原神 10258-4001 错误
+ */
+HWND FindHandleByWindowName() {
+  HWND handle = FindWindowA("UnityWndClass", "原神");
+  if (handle != 0) {
+      return handle;
+  }
+  
+  handle = FindWindowA("UnityWndClass", "Genshin Impact");
+  if (handle != 0) {
+      return handle;
+  }
+  
+  handle = FindWindowA("Qt5152QWindowIcon", "云·原神");
+  if (handle != 0) {
+      return handle;
+  }
+  
+  return NULL;
 }
 
 /* 获取整个屏幕的截图 */
