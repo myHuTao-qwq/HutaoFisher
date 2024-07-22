1839f773587fa56c1ae5fe61729c62ed87e56238
myHuTao-qwq <infinitesimal.q@gmail.com>
2024-07-22 19:51:11 +0800

    用直接计算替代了rodnet.cpp中的牛顿法求解

--- src/rodnet.cpp
+++ src/rodnet.cpp
@@ -31,73 +31,6 @@ const double offset[FISH_CLASS_NUM] = {0.4, 0.2,  0.4, 0,   0.3, 0.3,
                                        0.3, 0.15, 0.5, 0.5, 0.5, 0.5};
 #endif
 
-// dst = {a,b,v}
-void f(double* dst, double* x, double* y) {
-  double y0 = x[0], z0 = x[1], t = x[2];
-  double tmp = (y0 + t * z0) * (y0 + t * z0) - 1;
-  dst[0] = sqrt((1 + t * t) / tmp) - y[0];
-  dst[1] = (1 + t * t) * z0 / tmp - y[1];
-  dst[2] = ((t * t - 1) * y0 * z0 + t * (y0 * y0 - z0 * z0 - 1)) / tmp - y[2];
-  return;
-}
-
-void dfInv(double* dst, double* x) {
-  double y0 = x[0], z0 = x[1], t = x[2];
-  double tmp1 = (y0 + t * z0) * (y0 + t * z0) - 1;
-  double tmp2 = 1 + t * t;
-  dst[0] = (1 - y0 * y0 + z0 * z0) / y0 * sqrt(tmp1 / tmp2);
-  dst[1] = -z0 * (y0 * y0 + t * (t * (1 + z0 * z0) + 2 * y0 * z0)) / tmp2 / y0;
-  dst[2] = ((t * t - 1) * y0 * z0 + t * (y0 * y0 - z0 * z0 - 1)) / y0 / tmp2;
-  dst[3] = -2 * z0 * sqrt(tmp1 / tmp2);
-  dst[4] = tmp1 / tmp2;
-  dst[5] = 0;
-  dst[6] = -z0 / y0 * sqrt(tmp1 / tmp2);
-  dst[7] = (y0 + t * z0) * (y0 + t * z0) / y0;
-  dst[8] = 1 + t * z0 / y0;
-  return;
-}
-
-bool NewtonRaphson(void f(double* dst, double* x, double* y),
-                   void dfInv(double* dst, double* x), double* dst, double* y,
-                   double* init, int n, int maxIter, double eps) {
-  double* f_est = new double[n];
-  double* df_inv = new double[n * n];
-  double* x = new double[n];
-  double err;
-
-  memcpy(x, init, n * sizeof(double));
-
-  for (int iter = 0; iter < maxIter; iter++) {
-    err = 0;
-    f(f_est, x, y);
-    for (int i = 0; i < n; i++) {
-      err += abs(f_est[i]);
-    }
-
-    if (err < eps) {
-      memcpy(dst, x, n * sizeof(double));
-      delete f_est;
-      delete df_inv;
-      delete x;
-      // printf("Newton-Raphson solver converge after %d steps, err: %lf !\n",
-      //        iter, err);
-      return true;
-    }
-
-    dfInv(df_inv, x);
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        x[i] -= df_inv[n * i + j] * f_est[j];
-      }
-    }
-  }
-  delete f_est;
-  delete df_inv;
-  delete x;
-  return false;
-}
-
 void softmax(double* dst, double* x, int n) {
   double sum = 0;
   for (int i = 0; i < n; i++) {
@@ -117,7 +50,8 @@ int _getRodState(rodInput input) {
   b = (input.rod_y2 - input.rod_y1) / 2 / alpha;
 
   if (a < b) {
-    std::swap(a, b);  // when vision is almost vertical this can cause error
+    b = sqrt(a * b);
+    a = b + 1e-6;
   }
 
   v0 = (288 - (input.rod_y1 + input.rod_y2) / 2) / alpha;
@@ -125,18 +59,13 @@ int _getRodState(rodInput input) {
   u = (input.fish_x1 + input.fish_x2 - input.rod_x1 - input.rod_x2) / 2 / alpha;
   v = (288 - (input.fish_y1 + input.fish_y2) / 2) / alpha;
 
-  double y0z0t[3], abv0[3] = {a, b, v0},
-                   init[3] = {30, 15, 1};  // empirical value
-
-  double solveSuccess =
-      NewtonRaphson(f, dfInv, y0z0t, abv0, init, 3, 1000, 1e-6);
-
-  if (!solveSuccess) {
-    return -1;
-  }
-
-  double y0 = y0z0t[0], z0 = y0z0t[1], t = y0z0t[2];
+  double y0, z0, t;
   double x, y, dist;
+
+  y0 = sqrt(pow(a, 4) - b * b + a * a * (1 - b * b - v0 * v0)) / (a * a);
+  z0 = b / (a * a);
+  t = a * a * (y0 * b + v0) / (a * a - b * b);
+
   x = u * (z0 + dz[input.fish_label]) * sqrt(1 + t * t) / (t - v);
   y = (z0 + dz[input.fish_label]) * (1 + t * v) / (t - v);
   dist = sqrt(x * x + (y - y0) * (y - y0));
